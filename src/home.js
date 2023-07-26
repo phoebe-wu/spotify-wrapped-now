5482aca52350194c8783bd26f12c095cb8b0bf1e
phoebewu <phoebe.aqwu@gmail.com>
2023-07-26 00:22:29 -0700

    basic split screen

--- src/home.js
+++ src/home.js
@@ -2,15 +2,16 @@ import { useState, useEffect } from "react";
 import './home.css'
 import axios from 'axios';
 import Sidebar from "./sidebar.js";
+import Wrapped from "./wrapped.js";
 
-const SideBar = () => <div>SideBar</div>
-const TopItems = () => <div>TopItems</div>
+// const SideBar = () => <div>SideBar</div>
+// const Wrapped = () => <div>Wrapped</div>
 
 export default function Home() {
     return (
         <div class="home-page">
             <div><Sidebar/></div>
-            <div class='home-container'>Two MAIN</div>
+            <div><Wrapped/></div>
         </div>
     )
 }
---
+++ src/wrapped.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './wrapped.css';
+import './app.css';
+
+export default function Wrapped() {
+    return <div className = 'home-container'>
+        One WRAPPED NOW
+        </div>  
+}
